d4c36cad519da5edb6c725d7e7b2ddf2d2d6729b
Diego Ramos <diego.coderamos@gmail.com>
2019-09-16 15:25:53 -0300

    update New post layout

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import './New.scss';
 
 class New extends Component {
   render() {
     return (
-      <div>
-        <h1>new/</h1>
-      </div>
+      <form id="new-post">
+        <input type="file" />
+        <input type="text" name="author" placeholder="autor do post" />
+        <input type="text" name="place" placeholder="local do post" />
+        <input type="text" name="description" placeholder="descrição do post" />
+        <input type="text" name="hashtags" placeholder="hashtags do post" />
+        <button type="submit">enviar</button>
+      </form>
     );
   }
 }
