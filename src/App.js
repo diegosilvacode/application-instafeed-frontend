0e7d6c668250f3d9f1293de960f8697fa6f28d62
Diego Ramos <diego.coderamos@gmail.com>
2019-09-16 12:20:52 -0300

    create Header component and configure Links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
+import Header from './components/Header';
 
 import './app.scss';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
     </BrowserRouter>
   );
